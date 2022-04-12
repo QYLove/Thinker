525afac1d4f0f731f601f8191729073fe7819685
QYLove <wukeyang0209@163.com>
2022-04-12 13:52:41 +0800

    首次提交

--- app/src/main/java/com/sun/thinker/MainActivity.kt
+++ app/src/main/java/com/sun/thinker/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sun.thinker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import butterknife.ButterKnife
+import com.tele.tscheckeye.model.lib_tools.util.LogUtil
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ButterKnife.bind(this)
+
+        LogUtil.logD("我能使用了")
     }
 }
